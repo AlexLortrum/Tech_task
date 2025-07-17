9b4412b8bd4816767088309913164b3f5f421a31
AlexLortrum <alexanderilyushechky@yandex.ru>
2025-07-17 16:35:07 +0000

    Update download_page.py

--- pages/download_page.py
+++ pages/download_page.py
@@ -10,7 +10,7 @@ class Download_page(Base_page):
     def download_web_installer(self):
         try:
             self.find_element(*self.web_installer).click()
-            logging.info("Download_page.download_web_installer(): success")
+            self.logger.info("Download_page.download_web_installer(): success")
             return 0
         except:
             logging.exception("Download_page.download_web_installer(): error")
@@ -18,12 +18,12 @@ class Download_page(Base_page):
 
     def get_web_installer_name(self):
         element = self.find_element(*self.web_installer).get_attribute("href")
-        logging.info("Download_page.get_web_installer_name(): success")
+        self.logger.info("Download_page.get_web_installer_name(): success")
         return element.split('/')[-1]
 
     def get_web_installer_size(self):
         text = self.find_element(*self.web_installer).text
         text = text.split(" ")[2:]
         text[1] = text[1].replace(")", "")
-        logging.info("Download_page.get_web_installer_size(): success")
+        self.logger.info("Download_page.get_web_installer_size(): success")
         return text
